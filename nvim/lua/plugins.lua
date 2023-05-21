82f5f667a681c04bef6d63f6df65628118174c48
Andrii Kohut <kogut.andriy@gmail.com>
2023-05-21 17:40:00 +0300

    add sql to treesitter plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -85,7 +85,7 @@ require("lazy").setup({
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "rust", "typescript" },
+        ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "rust", "typescript", "sql" },
         highlight = { enable = true },
         incremental_selection = { enable = true },
         indent = { enable = true }
